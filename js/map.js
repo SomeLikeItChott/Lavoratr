c448a5257937a99ec4937293971f8d9cbc2cea2c
Stardusty <williamhamoore@gmail.com>
2014-02-23 15:09:45 -0600

    Changed map.js to make usermarker a normal marker, and fixed globilzation issues

--- js/map.js
+++ js/map.js
@@ -1,5 +1,26 @@
 
 var currentPosition ={lat:0, lng:0}
+
+var tIcon = L.icon({
+    iconUrl: 'img/Toilet.png',
+    shadowUrl: 'img/Toilet_drop.png',
+    iconSize:     [30, 30], // size of the icon
+    shadowSize:   [16, 32], // size of the shadow
+    iconAnchor:   [22, 20], // point of the icon which will correspond to marker's location
+    shadowAnchor: [9, 22],  // the same for the shadow
+    popupAnchor:  [-7, -20] // point from which the popup should open relative to the iconAnchor
+    });
+
+var uIcon = L.icon({
+  iconUrl: 'img/peeman.png',
+  shadowUrl: 'img/peeman.png',
+  iconSize: [20, 40],
+  shadowSize: [20, 40],
+  iconAnchor: [10, 30],
+  shadowAnchor: [10, 30],
+  popupAnchor: [-7, -5]
+});
+
 jQuery(document).ready(function() {
   
   venue_latlng = [36.1509, -95.9464];
@@ -13,12 +34,12 @@ jQuery(document).ready(function() {
 
   var distWatchID,
       posOptions = {enableHighAccuracy: true},
-      user_marker = L.userMarker([0,0], {pulsing:true, accuracy:100,smallIcon:true}).addTo(map),
+      user_marker = L.marker([0,0], {icon: uIcon}).addTo(map),
       new_position = function(pos) {
-          var pos_accuracy = pos.coords.accuracy;
+          //var pos_accuracy = pos.coords.accuracy;
           map.user_position = new L.LatLng(pos.coords.latitude, pos.coords.longitude);
           user_marker.setLatLng(map.user_position);
-          user_marker.setAccuracy(pos_accuracy);
+          //user_marker.setAccuracy(pos_accuracy);
           map.setView(map.user_position,18)
 
       },
@@ -26,17 +47,18 @@ jQuery(document).ready(function() {
           if (err){
               console.warn('ERROR(' + err.code + '): ' + err.message);
           }
-      },
+      }
+
+      var fireRef = new Firebase('https://lavoratr.firebaseio.com/');
+    fireRef.on('child_added', function(snapshot) {
+        var latData = (snapshot.val()).latitude;
+        var longData = (snapshot.val()).longitude;
+        L.marker([latData,longData], {icon: tIcon}).addTo(map);
+    });
+    
+    distWatchID = navigator.geolocation.watchPosition(new_position, appPosFail, posOptions);
      
       //beginning of things that Sam added
-      tIcon = L.icon({
-    iconUrl: 'img/Toilet.png',
-    shadowUrl: 'img/Toilet_drop.png',
-    iconSize:     [30, 30], // size of the icon
-    shadowSize:   [16, 32], // size of the shadow
-    iconAnchor:   [22, 20], // point of the icon which will correspond to marker's location
-    shadowAnchor: [9, 22],  // the same for the shadow
-    popupAnchor:  [-7, -20] // point from which the popup should open relative to the iconAnchor
 });
 //       tulsaLoc = L.marker([36.1522, -95.9464], {icon: tIcon}).addTo(map);
 //                   
@@ -69,17 +91,9 @@ jQuery(document).ready(function() {
 //     }
 // }
 // //copypaste end
-
-		var fireRef = new Firebase('https://lavoratr.firebaseio.com/');
-		fireRef.on('child_added', function(snapshot) {
-	  		var latData = (snapshot.val()).latitude;
-  			var longData = (snapshot.val()).longitude;
-  			L.marker([latData,longData], {icon: tIcon}).addTo(map);
-		});
-		
-	  distWatchID = navigator.geolocation.watchPosition(new_position, appPosFail, posOptions);       
+       
 
 
-});
+//});
   
   
